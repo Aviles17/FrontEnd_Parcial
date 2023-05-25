d9e6139b58f76857ec99c4dfa39ce0484fdeaa97
Aviles17 <santiaviles17@gmail.com>
2023-05-25 18:19:25 -0500

    Intento de mostrar lista de resultado

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -10,12 +10,26 @@ import {Book} from "../model/Book";
 export class BookListComponent implements OnInit {
   books: Book[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit() {
-    this.http.get<Book[]>('http://localhost:8080/GetLibro')
-      .subscribe((response: Book[]) => {
-        this.books = response;
+    this.http.get<any[]>('http://localhost:8080/GetLibro')
+      .subscribe((response: any[]) => {
+        try {
+          this.books = response.map((bookData: any) => {
+            return {
+              titulo: bookData.titulo,
+              referencia: bookData.referencia,
+              autor: bookData.autor,
+              precio: bookData.precio,
+              ubicacion: bookData.ubicacion
+            };
+          });
+          console.log(this.books);
+        } catch (error) {
+          console.error('Error parsing the response:', error);
+        }
       });
   }
 }
